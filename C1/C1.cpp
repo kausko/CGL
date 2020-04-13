a50bd89ee74b74d5e42d5d2557324903c792309d
Kaustubh Odak <53346282+kausko@users.noreply.github.com>
2020-04-13 16:48:38 +0530

    Update C1.cpp

--- C1/C1.cpp
+++ C1/C1.cpp
@@ -20,135 +20,118 @@ float angle;
 
 int choice,choiceRot;
 
-void setIdentityM(Matrix4 m)
-{
-for(int i=0;i<4;i++)
-    for(int j=0;j<4;j++)
-        m[i][j]=(i==j);
+void setIdentityM(Matrix4 m){
+    for(int i=0;i<4;i++)
+        for(int j=0;j<4;j++)
+            m[i][j]=(i==j);
 }
 
-void translate(int tx,int ty,int tz)
-{
+void translate(int tx,int ty,int tz){
+    for(int i=0;i<8;i++){
+        output[i][0]=input[i][0]+tx;
+        output[i][1]=input[i][1]+ty;
+        output[i][2]=input[i][2]+tz;
+    }
+}
 
-for(int i=0;i<8;i++)
-{
-output[i][0]=input[i][0]+tx;
-output[i][1]=input[i][1]+ty;
-output[i][2]=input[i][2]+tz;
-}
-}
-void scale(int sx,int sy,int sz)
-{
+void scale(int sx,int sy,int sz){
     theMatrix[0][0]=sx;
     theMatrix[1][1]=sy;
     theMatrix[2][2]=sz;
 }
-void RotateX(float angle) //Parallel to x
-{
 
- angle = angle*3.142/180;
-  theMatrix[1][1] = cos(angle);
- theMatrix[1][2] = -sin(angle);
- theMatrix[2][1] = sin(angle);
- theMatrix[2][2] = cos(angle);
-
-}
-void RotateY(float angle) //parallel to y
-{
-
- angle = angle*3.14/180;
- theMatrix[0][0] = cos(angle);
- theMatrix[0][2] = -sin(angle);
- theMatrix[2][0] = sin(angle);
- theMatrix[2][2] = cos(angle);
-
-}
-void RotateZ(float angle) //parallel to z
-{
-
- angle = angle*3.14/180;
- theMatrix[0][0] = cos(angle);
- theMatrix[0][1] = sin(angle);
- theMatrix[1][0] = -sin(angle);
- theMatrix[1][1] = cos(angle);
-
+//Parallel to x
+void RotateX(float angle) {
+    angle = angle*3.142/180;
+    theMatrix[1][1] = cos(angle);
+    theMatrix[1][2] = -sin(angle);
+    theMatrix[2][1] = sin(angle);
+    theMatrix[2][2] = cos(angle);
 }
 
-void multiplyM()
-{
-//We Don't require 4th row and column in scaling and rotation
-//[8][3]=[8][3]*[3][3] //4th not used
-for(int i=0;i<8;i++)
- {
-    for(int j=0;j<3;j++)
-    {
-        output[i][j]=0;
-        for(int k=0;k<3;k++)
-        {
-            output[i][j]=output[i][j]+input[i][k]*theMatrix[k][j];
+//parallel to y
+void RotateY(float angle) {
+    angle = angle*3.14/180;
+    theMatrix[0][0] = cos(angle);
+    theMatrix[0][2] = -sin(angle);
+    theMatrix[2][0] = sin(angle);
+    theMatrix[2][2] = cos(angle);
+}
+
+//parallel to z
+void RotateZ(float angle) {
+    angle = angle*3.14/180;
+    theMatrix[0][0] = cos(angle);
+    theMatrix[0][1] = sin(angle);
+    theMatrix[1][0] = -sin(angle);
+    theMatrix[1][1] = cos(angle);
+}
+
+void multiplyM(){
+
+    for(int i=0;i<8;i++){
+        for(int j=0;j<3;j++){
+            output[i][j]=0;
+            for(int k=0;k<3;k++){
+                output[i][j]=output[i][j]+input[i][k]*theMatrix[k][j];
+            }
         }
     }
 }
-
+void Axes(void){
+    glColor3f (0.0, 0.0, 0.0);               // Set the color to BLACK
+    glBegin(GL_LINES);                       // Plotting X-Axis
+    glVertex2s(-1000 ,0);
+    glVertex2s( 1000 ,0);
+    glEnd();
+    glBegin(GL_LINES);                       // Plotting Y-Axis
+    glVertex2s(0 ,-1000);
+    glVertex2s(0 , 1000);
+    glEnd();
 }
-void Axes(void)
-{
- glColor3f (0.0, 0.0, 0.0);               // Set the color to BLACK
- glBegin(GL_LINES);                       // Plotting X-Axis
- glVertex2s(-1000 ,0);
- glVertex2s( 1000 ,0);
- glEnd();
- glBegin(GL_LINES);                       // Plotting Y-Axis
- glVertex2s(0 ,-1000);
- glVertex2s(0 , 1000);
- glEnd();
-}
-void draw(float a[8][3])
-{
-
+void draw(float a[8][3]){
 
     glBegin(GL_QUADS);
-     glColor3f(0.7,0.4,0.5); //behind
+    glColor3f(0.7,0.4,0.5); //behind
     glVertex3fv(a[0]);
     glVertex3fv(a[1]);
     glVertex3fv(a[2]);
     glVertex3fv(a[3]);
 
     glColor3f(0.8,0.2,0.4);  //bottom
-   glVertex3fv(a[0]);
-   glVertex3fv(a[1]);
-   glVertex3fv(a[5]);
-   glVertex3fv(a[4]);
+    glVertex3fv(a[0]);
+    glVertex3fv(a[1]);
+    glVertex3fv(a[5]);
+    glVertex3fv(a[4]);
 
-   glColor3f(0.3,0.6,0.7); //left
-  glVertex3fv(a[0]);
-  glVertex3fv(a[4]);
-  glVertex3fv(a[7]);
-  glVertex3fv(a[3]);
+    glColor3f(0.3,0.6,0.7); //left
+    glVertex3fv(a[0]);
+    glVertex3fv(a[4]);
+    glVertex3fv(a[7]);
+    glVertex3fv(a[3]);
 
-  glColor3f(0.2,0.8,0.2);  //right
- glVertex3fv(a[1]);
- glVertex3fv(a[2]);
- glVertex3fv(a[6]);
- glVertex3fv(a[5]);
+    glColor3f(0.2,0.8,0.2);  //right
+    glVertex3fv(a[1]);
+    glVertex3fv(a[2]);
+    glVertex3fv(a[6]);
+    glVertex3fv(a[5]);
 
- glColor3f(0.7,0.7,0.2); //up
-glVertex3fv(a[2]);
-glVertex3fv(a[3]);
-glVertex3fv(a[7]);
-glVertex3fv(a[6]);
+    glColor3f(0.7,0.7,0.2); //up
+    glVertex3fv(a[2]);
+    glVertex3fv(a[3]);
+    glVertex3fv(a[7]);
+    glVertex3fv(a[6]);
 
-glColor3f(1.0,0.1,0.1);
-glVertex3fv(a[4]);
-glVertex3fv(a[5]);
-glVertex3fv(a[6]);
-glVertex3fv(a[7]);
+    glColor3f(1.0,0.1,0.1);
+    glVertex3fv(a[4]);
+    glVertex3fv(a[5]);
+    glVertex3fv(a[6]);
+    glVertex3fv(a[7]);
 
-glEnd();
+    glEnd();
 }
 
-void init()
-{
+void init(){
     glClearColor(1.0,1.0,1.0,1.0); //set backgrond color to white
     glOrtho(-454.0,454.0,-250.0,250.0,-250.0,250.0);
     // Set the no. of Co-ordinates along X & Y axes and their gappings
@@ -156,41 +139,39 @@ void init()
      // To Render the surfaces Properly according to their depths
 }
 
-void display()
-{
-glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-Axes();
-glColor3f(1.0,0.0,0.0);
-draw(input);
-setIdentityM(theMatrix);
-switch(choice)
-{
-case 1:
-    translate(tx,ty,tz);
-    break;
- case 2:
-    scale(sx,sy,sz);
-multiplyM();
-    break;
- case 3:
-    switch (choiceRot) {
-    case 1:
-        RotateX(angle);
-        break;
-    case 2: RotateY(angle);
-        break;
-    case 3:
-        RotateZ(angle);
-        break;
-    default:
-        break;
+void display(){
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    Axes();
+    glColor3f(1.0,0.0,0.0);
+    draw(input);
+    setIdentityM(theMatrix);
+    
+    switch(choice){
+        case 1:
+            translate(tx,ty,tz);
+            break;
+         case 2:
+            scale(sx,sy,sz);
+            multiplyM();
+            break;
+         case 3:
+            switch (choiceRot) {
+                case 1:
+                    RotateX(angle);
+                    break;
+                case 2: RotateY(angle);
+                    break;
+                case 3:
+                    RotateZ(angle);
+                    break;
+                default:
+                    break;
+            }
+            multiplyM();
+                break;
     }
-multiplyM();
-    break;
-}
-
-draw(output);
-glFlush();
+    draw(output);
+    glFlush();
 }
 
 int main(int argc, char** argv)
@@ -204,40 +185,40 @@ int main(int argc, char** argv)
     cout<<"Enter your choice number:\n1.Translation\n2.Scaling\n3.Rotation\n=>";
     cin>>choice;
     switch (choice) {
-    case 1:
-        cout<<"\nEnter Tx,Ty &Tz: \n";
-        cin>>tx>>ty>>tz;
-        break;
-    case 2:
-        cout<<"\nEnter Sx,Sy & Sz: \n";
-        cin>>sx>>sy>>sz;
-        break;
-    case 3:
-        cout<<"Enter your choice for Rotation about axis:\n1.parallel to X-axis."
-             <<"(y& z)\n2.parallel to Y-axis.(x& z)\n3.parallel to Z-axis."
-              <<"(x& y)\n =>";
-        cin>>choiceRot;
-        switch (choiceRot) {
         case 1:
-            cout<<"\nENter Rotation angle: ";
-            cin>>angle;
+            cout<<"\nEnter Tx,Ty &Tz: \n";
+            cin>>tx>>ty>>tz;
             break;
         case 2:
-            cout<<"\nENter Rotation angle: ";
-            cin>>angle;
+            cout<<"\nEnter Sx,Sy & Sz: \n";
+            cin>>sx>>sy>>sz;
             break;
         case 3:
-            cout<<"\nENter Rotation angle: ";
-            cin>>angle;
+            cout<<"Enter your choice for Rotation about axis:\n1.parallel to X-axis."
+                 <<"(y& z)\n2.parallel to Y-axis.(x& z)\n3.parallel to Z-axis."
+                  <<"(x& y)\n =>";
+            cin>>choiceRot;
+            switch (choiceRot) {
+                case 1:
+                    cout<<"\nENter Rotation angle: ";
+                    cin>>angle;
+                    break;
+                case 2:
+                    cout<<"\nENter Rotation angle: ";
+                    cin>>angle;
+                    break;
+                case 3:
+                    cout<<"\nENter Rotation angle: ";
+                    cin>>angle;
+                    break;
+                default:
+                    break;
+            }
             break;
         default:
             break;
         }
-        break;
-    default:
-        break;
-    }
     glutDisplayFunc(display);
     glutMainLoop();
-return 0;
+    return 0;
 }
